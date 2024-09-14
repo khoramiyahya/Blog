69c5c93cda7ab99984090e279f98bbe353535cef
y.khorami <y.khorami@samaninsurance.ir>
2024-09-14 08:20:34 +0330

    Push Changes

--- CoreLayer/obj/Debug/net8.0/CoreLayer.AssemblyInfo.cs
+++ CoreLayer/obj/Debug/net8.0/CoreLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoreLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc000629b5684c80783e867bc42cf7264b506cb2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+415ba086771a127d8cfec980749fc3e000ae892b")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoreLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoreLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- DataLayer/obj/Debug/net8.0/DataLayer.AssemblyInfo.cs
+++ DataLayer/obj/Debug/net8.0/DataLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc000629b5684c80783e867bc42cf7264b506cb2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+415ba086771a127d8cfec980749fc3e000ae892b")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
